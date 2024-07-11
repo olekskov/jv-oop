df32af912e8c457019dd879c42a6c99c9a9961b4
Kostya Pavlyk <koast59385@gmail.com>
2024-07-11 15:50:55 +0300

    corrected mistake

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -2,14 +2,10 @@ package core.basesyntax;
 
 public class MainApp {
     public static void main(String[] args) {
-        Machine bulldozer = new Bulldozer();
-        Machine excavator = new Excavator();
-        Machine truck = new Truck();
-
-        Machine[] arrayMachine = {bulldozer, excavator, truck};
-        for (int i = 0; i < arrayMachine.length; i++) {
-            arrayMachine[i].doWork();
-            arrayMachine[i].stopWork();
+        Machine[] machines = {new Bulldozer(), new Excavator(), new Truck()};
+        for (Machine machine : machines) {
+            machine.doWork();
+            machine.stopWork();
         }
     }
 }
